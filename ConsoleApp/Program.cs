8a2c679f94506cc1bd775996541894ba6596cf98
Santiago <=>
2024-07-31 21:56:28 -0300

    Verificacion de edad

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -4,12 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("¡Hola, a todo el mundo!");
+            Console.WriteLine("¡Hola, a todo el mundo! (mayor de 18 años)");
             Console.WriteLine("Ingrese su nombre: ");
             string nombre = Console.ReadLine();
             if (!string.IsNullOrEmpty(nombre))
             {
                 Console.WriteLine($"Hola, {nombre}");
+            }
+            Console.WriteLine("Necesitamos verificar si es mayor de edad");
+            Console.WriteLine("Por favor, Ingrese su edad: ");
+            int edad = int.Parse(Console.ReadLine());
+            if (edad >= 18)
+            {
+                Console.WriteLine("Bienvenido!");
+            }
+            else
+            {
+                Console.WriteLine("No puede ingresar");
             }
         }
     }
